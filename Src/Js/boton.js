c632fbcf41a3543442a3f443c1adfa83a5d44d41
JehuCalle <josiasandres56@gmail.com>
2023-11-27 18:15:29 -0300

    Animación de la tarjeta

--- Src/Js/boton.js
+++ Src/Js/boton.js
@@ -3,24 +3,42 @@ console.log("😼");
 const primerDiv = document.querySelector("#primerDiv");
 const segundoDiv = document.querySelector("#segundoDiv");
 const tercerDiv = document.querySelector("#tercerDiv");
-const btnIniciarSecuencia = document.querySelector("#btnIniciarSecuencia");
+const btnCentroIzquierda = document.querySelector("#btnCentroIzquierda");
+const btnCentroDerecha = document.querySelector("#btnCentroDerecha");
 
-let contador = 0;
+let contPag1 = 0;
+btnCentroDerecha.addEventListener('click', (e) => {
+  e.preventDefault
+  const pagina1 = document.querySelector('.pagina1');
+  const pagina2 = document.querySelector('.pagina2');
+  console.log("0")
+  if (contPag1 === 0) {
+    pagina1.style.transform = 'rotateY(-180deg)';
+    pagina2.style.transform = 'rotateY(-180deg)';
+    console.log("1");
+    contPag1 += 1;
+  } else {
+    pagina1.style.transform = 'rotateY(0deg)';
+    pagina2.style.transform = 'rotateY(0deg)';
+    console.log("2");
+    contPag1 -= 1;
+  }
+});
 
-function secuencia(){
-  segundoDiv.classList.add("d-none");
-  tercerDiv.classList.remove("d-none");
-};
+let contPag2 = 0;
+btnCentroIzquierda.addEventListener('click', (e) => {
+  const pagina3 = document.querySelector('.pagina3');
+  const pagina4 = document.querySelector('.pagina4');
 
-btnIniciarSecuencia.addEventListener( 'click', (e) => {
-  e.preventDefault();
-
-  if( contador === 0 ){
-    primerDiv.classList.add("d-none");
-    segundoDiv.classList.remove("d-none");
-    setTimeout(secuencia, 5000);
-    contador += 1;
-  } else if( contador !== 0){
-    console.log("Fin");
-  };
+  if (contPag2 === 0) {
+    pagina3.style.transform = 'rotateY(180deg)';
+    pagina4.style.transform = 'rotateY(180deg)';
+    console.log("3");
+    contPag2 += 1;
+  } else {
+    pagina3.style.transform = 'rotateY(0deg)';
+    pagina4.style.transform = 'rotateY(0deg)';
+    console.log("4");
+    contPag2 -=1
+  }
 });
